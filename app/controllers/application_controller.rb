7bd0ab29e6daeef0a957962b9199cdc8d9c84995
thengugi <thengugi@gmail.com>
2018-12-20 13:36:42 +0530

    static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+	#index page
+	def index
+		render html: 'hello'
+	end
 end
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  
+  #Action for home page
+  def home
+  end
+
+  #Action for help page
+  def help
+  end
+end
