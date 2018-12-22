dfd944f3f47244938f45d967bbb62375ebcbc473
thengugi <thengugi@gmail.com>
2018-12-22 12:08:25 +0530

    latest updates

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,19 @@
+class StaticPagesController < ApplicationController
+  
+  #Action for home page
+  def home
+  end
+
+  #Action for help page
+  def help
+  end
+
+  #About Page
+  def about
+  end
+
+  #contact page
+  def contact
+  	
+  end
+end
